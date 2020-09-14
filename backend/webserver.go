0e6cfacf09ce24bbaab7592930fb18013dbd9fcc
Thomas Granbohm <thomas.granbohm@gmail.com>
2020-09-14 16:41:14 +0200

    ⚡️ Better error handling, sort of

--- backend/webserver.go
+++ backend/webserver.go
@@ -3,11 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"strconv"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./files")))
+	port := 5000
+	filePath, _ := os.Getwd()
+	fullPath := path.Join(filePath, "./files")
 
-	log.Println("Listening on :5000")
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	// https://gist.github.com/mattes/d13e273314c3b3ade33f Hederligt stulet
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		log.Println("Files directory does not exist in " + filePath)
+		log.Println("Please create files directory and restart.")
+		return
+	}
+	http.Handle("/", http.FileServer(http.Dir(fullPath)))
+
+	log.Println("Listening on :" + strconv.Itoa(port))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
